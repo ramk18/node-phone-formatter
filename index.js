4c9062286ebe8c54439c14287d8902ab5713fb58
ramk18 <mail@ramk.co.in>
2016-11-29 13:21:53 +0530

    empty postfix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = {
     var m = phoneNumber.match(/^(\+?[0-9]{1,3})?[-. ]?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})[-. ]?(.*)?$/);
     phone.prefix = (m[1])?m[1]:'';
     phone.number = m[2]+m[3]+m[4];
-    phone.postfix = m[5];
+    phone.postfix = (m[2])?m[2]:'';
     
     return phone;
 
@@ -18,7 +18,7 @@ module.exports = {
 
   format: function format(phoneNumber, formatString, options) {
     // Normalize the phone number first unless not asked to do so in the options
-    phoneNumber = this.normalize(phoneNumber)
+    phoneNumber = this.normalize(phoneNumber);
     
     for ( var i = 0, l = phoneNumber.number.length; i < l; i++ ) {
       formatString = formatString.replace("N", phoneNumber.number[i]);
